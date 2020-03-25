4c5c56c44a6f8cebd1c830e846df23f87866e8a3
Max Orok <maxwellorok@gmail.com>
2020-03-24 22:10:58 -0400

    Readme polish

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
-//!
-//! Some functions for cin like input from stdin for all types that implement FromStr
+//! `cin`-like input from `stdin` for all types that implement `FromStr`.
 //!
 //! Useful for quick prototyping and debugging without passing any state around.
 //!
-//! (And for people who complain that input in rust is too verbose)
+//! (And for people who complain that input in rust is too verbose.)
 //!
 //! It stores a buffer of the last line and tries to consume it first.
-//! Blocks until it finds any sequence of non whitespace characters
+//! It will block until it finds any sequence of non-whitespace characters.
 //!
-//! Depends on the [lazy_static](https://docs.rs/lazy_static) crate for storing global state
+//! Depends on the [lazy_static](https://docs.rs/lazy_static) crate for storing global state.
 //!
 //! ## Example
 //! ```no_run
@@ -31,21 +30,20 @@
 //!
 //! ## Thread safety
 //!
-//! Rcin is thread safe, but all threads will share one buffer.
-//! (Parallel input from stdin is not a usable thing, is it?)
+//! `Rcin` is thread safe, but all threads share one buffer.
+//! (Parallel input from `stdin` is not a usable thing, is it?)
 //!
-//! `pause` is __not__ a common lock for all threads
+//! `pause` is __not__ a common lock for all threads.
 //!
 //! ## Corner case
 //!
-//! Does __not__ read the input char by char like cin and requires whitespaces between groups
+//! Does __not__ read the input char by char like cin and requires whitespace between groups.
 //!
 //! Reading an int:
 //! ```text
-//! C++: 17GRABAGE => 17 //perfectly fine lol
+//! C++: 17GARBAGE => 17 // perfectly fine lol
 //! RCin: 17GARBAGE => None
 //! ```
-//!
 use std::cell::{RefCell, RefMut, Ref};
 use std::io::{BufRead, Write};
 use std::str::FromStr;
